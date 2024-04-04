34be7222eba15ac92b4fe3560ba560868893424b
ReynaldiRP <rizkypratama471@gmail.com>
2024-04-04 21:32:02 +0700

    first init

---
+++ resources/js/Pages/Components/HelloWorld2.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>this is Hello World 2</div>
+</template>
+
+<script setup></script>
---
+++ resources/js/Pages/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="min-h-screen flex justify-center items-center">
+        <h1 class="bg-red-600 p-4 rounded-xl">Hello, {{ name }}</h1>
+    </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    name: String,
+});
+</script>
